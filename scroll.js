cb0aa55e76e5f630325ab0c3df68aac29d48a868
RJ Skills <tenure.emerita-0z@icloud.com>
2026-02-14 15:44:50 -0500

    Mid February Update Part 1

--- scroll.js
+++ scroll.js
@@ -1,19 +1,11 @@
-// Get the button
-let mybutton = document.getElementById("backtotopBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+       $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 1500);
+    return false;
+  });
